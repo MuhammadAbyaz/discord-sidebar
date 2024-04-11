125c86bebe6d6ea23ea1a383782ee5807faa93f2
Abyaz <58655276+MuhammadAbyaz@users.noreply.github.com>
2024-04-12 02:30:36 +0500

    feat: sidebar completed

---
+++ src/SideBar.jsx
@@ -0,0 +1,23 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
+const SideBar = () => {
+  return (
+    <div className="fixed left-0 top-0 m-0 flex h-screen w-16 flex-col bg-cyan-950 text-white shadow-lg">
+      <SideBarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<BsPlus size="32" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcon icon={<FaPoo size="20" />} />
+    </div>
+  );
+};
+
+const SideBarIcon = ({ icon, text = "tool-tip 💡" }) => {
+  return (
+    <div className="sidebar-icon group">
+      {icon}
+      <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+    </div>
+  );
+};
+export default SideBar;
---
+++ src/application.jsx
@@ -0,0 +1,8 @@
+import SideBar from "./SideBar";
+export function Application() {
+  return (
+    <div className="flex">
+      <SideBar />
+    </div>
+  );
+}
